0215e9a3ce1a6ccfc208df30f421cdc371dcc91b
wany0011 <wanyong_sin@hotmail.com>
2022-06-04 15:22:35 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -15,9 +15,13 @@ from flask import render_template,request
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        return(render_template("index.html", result="temp"))
+        text = request.form.get("text")
+        print(text)
+        r1 = TextBlob(text).sentiment
+        r2 = classifier(text)
+        return(render_template("index.html", result1=r1, result2=r2))
     else:
-        return(render_template("index.html", result="waiting..."))
+        return(render_template("index.html", result1="2", result2="2"))
 
 if __name__=="__main__":
     app.run()
